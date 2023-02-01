52d9a6816e8495a8517962fe4171aec493f495f7
Steve Lacey <steve@steve.ly>
2023-02-01 17:03:48 +0800

    Support dev packages and +/- in pyenvdiff

--- python/pipenvdiff.py
+++ python/pipenvdiff.py
@@ -1,27 +1,32 @@
-# https://gist.github.com/ddahan/215472a4aa1805160aba0a77902e40da
 import argparse
 import json
 import re
 
-parser = argparse.ArgumentParser(description="Compare two Pipfile.lock files")
-parser.add_argument(
-    "file1", type=str, nargs="+", help="File 1 (the old one) to be compared"
-)
-parser.add_argument(
-    "file2", type=str, nargs="+", help="File 2 (the new one) to be compared"
-)
+parser = argparse.ArgumentParser(description="Compare two Pipfile.lock files.")
+parser.add_argument("base", type=str, help="The base lock file to be compared.")
+parser.add_argument("head", type=str, help="The head lock file to be compared.")
 
 args = parser.parse_args()
-file_1, file_2 = args.file1[0], args.file2[0]
 
-with open(file_1) as data_file_1, open(file_2) as data_file_2:
-    data_1_loaded, data_2_loaded = json.load(data_file_1), json.load(data_file_2)
+with open(args.base) as base_file, open(args.head) as head_file:
+    base, head = json.load(base_file), json.load(head_file)
 
-packages_1, packages_2 = data_1_loaded["default"], data_2_loaded["default"]
+changes = []
 
-for package_name, package_info in packages_1.items():
-    if package_name in packages_2.keys():
-        p1_version = re.sub("[=]", "", package_info.get("version", ""))
-        p2_version = re.sub("[=]", "", packages_2[package_name].get("version", ""))
-        if not p1_version == p2_version:
-            print(f"{package_name}: {p1_version} -> {p2_version}")
+for group in (set(base.keys()) | set(head.keys())) - {"_meta"}:
+    base_packages, head_packages = base[group], head[group]
+
+    for name, info in base_packages.items():
+        if name not in head_packages.keys():
+            changes.append(f"{name} removed")
+            continue
+        base_version = re.sub(r"[=]", "", info.get("version", ""))
+        head_version = re.sub(r"[=]", "", head_packages[name].get("version", ""))
+        if not base_version == head_version:
+            changes.append(f"{name}: {base_version} -> {head_version}")
+
+    for name, info in head_packages.items():
+        if name not in base_packages.keys():
+            changes.append(f"{name} added")
+
+print("\n".join(sorted(changes)))
